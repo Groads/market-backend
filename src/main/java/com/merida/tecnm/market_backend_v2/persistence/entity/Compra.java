000fe6edfb2dd2905e86151d7e2c25a9f2bc0c95
kevin <kevinbarato7@gmail.com>
2026-09-17 17:50:00 -0600

    feat(persistence):add Producto and Compra JPA entities

---
+++ src/main/java/com/merida/tecnm/market_backend_v2/persistence/entity/Compra.java
@@ -0,0 +1,22 @@
+package com.merida.tecnm.market_backend_v2.persistence.entity;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "compras")
+public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name="id_compra")
+    private Integer idCompra;
+
+    @Column (name="id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column (name="medio_pago")
+    private String medioPago;
+    private String comentario;
+    private String estado;
+}
